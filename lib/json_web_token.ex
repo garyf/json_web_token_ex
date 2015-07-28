6900ee7eac084cdc9d78e488f6e7cb3d6c29a3f8
Gary Fleshman <gfleshman@newforge-tech.com>
2015-07-27 21:44:49 -0400

    JsonWebToken sign/2, verify/2

--- lib/json_web_token.ex
+++ lib/json_web_token.ex
@@ -1,2 +1,36 @@
 defmodule JsonWebToken do
+  @moduledoc """
+  Top level interface, or API, for signing and verifying a JSON Web Token (JWT)
+
+  see http://tools.ietf.org/html/rfc7519
+  """
+
+  alias JsonWebToken.Jwt
+
+  @doc """
+  Return a JSON Web Token (JWT), a string representing a set of claims as a JSON object that is
+  encoded in a JWS
+
+  ## Example
+      iex> claims = %{iss: "joe", exp: 1300819380, "http://example.com/is_root": true}
+      ...> key = "gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C"
+      ...> JsonWebToken.sign(claims, %{key: key})
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwiZXhwIjoxMzAwODE5MzgwfQ.Ktfu3EdLz0SpuTIMpMoRZMtZsCATWJHeDEBGrsZE6LI"
+
+  see http://tools.ietf.org/html/rfc7519#section-7.1
+  """
+  def sign(claims, options), do: Jwt.sign(claims, options)
+
+  @doc """
+  Return a JWT claims map if the JWT signature does verify, or an "Invalid" string otherwise
+
+  ## Example
+      iex> jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwiZXhwIjoxMzAwODE5MzgwfQ.Ktfu3EdLz0SpuTIMpMoRZMtZsCATWJHeDEBGrsZE6LI"
+      ...> key = "gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C"
+      ...> JsonWebToken.verify(jwt, %{key: key})
+      %{iss: "joe", exp: 1300819380, "http://example.com/is_root": true}
+
+  see http://tools.ietf.org/html/rfc7519#section-7.2
+  """
+  def verify(jwt, options), do: Jwt.verify(jwt, options)
 end
--- test/json_web_token_test.exs
+++ test/json_web_token_test.exs
@@ -1,7 +1,25 @@
 defmodule JsonWebTokenTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  doctest JsonWebToken
+
+  @hs256_key "gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C"
+  @claims %{iss: "joe", exp: 1300819380, "http://example.com/is_root": true}
+
+  defp sign_and_verify(options, claims \\ @claims) do
+    jwt = JsonWebToken.sign(claims, options)
+    assert claims === JsonWebToken.verify(jwt, options)
+  end
+
+  test "sign/2 jwt w default alg does verify/2" do
+    sign_and_verify(%{key: @hs256_key})
+  end
+
+  test "sign/2 w HS256 alg does verify/2" do
+    sign_and_verify(%{alg: "HS256", key: @hs256_key})
+  end
+
+  test "sign/2 w 'none' alg (and no key) does verify/2" do
+    sign_and_verify(%{alg: "none"})
   end
 end
