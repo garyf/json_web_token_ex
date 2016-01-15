91786ea8a7c77df80c171cf80165d56a1d872771
Phil Burrows <github@philburrows.com>
2016-01-15 00:48:13 -0600

    Allow RSA keys to be loaded w/o a file

--- lib/json_web_token/algorithm/rsa_util.ex
+++ lib/json_web_token/algorithm/rsa_util.ex
@@ -1,21 +1,33 @@
 defmodule JsonWebToken.Algorithm.RsaUtil do
   @moduledoc "Encryption keys for test"
 
+  @doc "Load an RSA private key from a string"
+  def private_key(key) do
+    {:RSAPrivateKey, :'two-prime', n, e, d, _p, _q, _e1, _e2, _c, _other} =
+      entry_decode(key)
+    [e, n, d]
+  end
+
   @doc "Load an RSA private key from a pem file"
   def private_key(path_to_keys, filename) do
-    {:RSAPrivateKey, :'two-prime', n, e, d, _p, _q, _e1, _e2, _c, _other} =
-      entry_decode(path_to_keys, filename)
-    [e, n, d]
+    pem_read(path_to_keys, filename)
+    |> private_key
+  end
+
+  @doc "Load an RSA public key from a string"
+  def public_key(key) do
+    {:RSAPublicKey, n, e} = entry_decode(key)
+    [e, n]
   end
 
   @doc "Load an RSA public key from a pem file"
   def public_key(path_to_keys, filename) do
-    {:RSAPublicKey, n, e} = entry_decode(path_to_keys, filename)
-    [e, n]
+    pem_read(path_to_keys, filename)
+    |> public_key
   end
 
-  defp entry_decode(path_to_keys, filename) do
-    pem_read(path_to_keys, filename)
+  defp entry_decode(key) do
+    key
     |> :public_key.pem_decode
     |> List.first
     |> :public_key.pem_entry_decode
